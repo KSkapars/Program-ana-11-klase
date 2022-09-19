2d841885971a3cb053774fb94ac64c3f0edc4b7a
KSkapars <112852043+KSkapars@users.noreply.github.com>
2022-09-19 09:59:36 +0300

    Add files via upload

---
+++ uzd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+int x;
+
+    cout<<"Skaitli no 10-20:";
+    cin>>x;
+if (x>9 and x<21){
+cout<<"Paldies";
+}
+else{
+cout<<"Nepareizi";
+
+
+}
+
+return 0;
+}
