1075ff655d75ce43a0a3e7970127298603ed43e3
KSkapars <112852043+KSkapars@users.noreply.github.com>
2022-09-19 10:00:07 +0300

    Add files via upload

---
+++ md 2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int x, y, k;
+cout<<" Ievadi malu garumus trijstruriem:";
+cin>>x>>y>>k;
+
+if (x +y  <=k  || x + k <= y || y + k <= x){
+  cout<<"neeksiste" <<endl;
+  }
+   else {
+   cout<<"eksiste" <<endl;
+
+
+   }
+
+
+
+
+
+
+
+}
